a5f7b7bf1406f963a74735eb08a1d3d4956fe80a
ilies ouldmenouer <ilyasdzair1@gmail.com>
2021-10-19 13:04:46 +0100

    init commit

--- UikitLivePreview/ViewController.swift
+++ UikitLivePreview/ViewController.swift
@@ -2,18 +2,40 @@
 //  ViewController.swift
 //  UikitLivePreview
 //
-//  Created by yassir on 19/10/2021.
+//  Created by ilies on 19/10/2021.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let TextLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        TextLabel.text = "Hello World , This is a real time UIkit Preview test"
+        TextLabel.translatesAutoresizingMaskIntoConstraints = false
+        TextLabel.numberOfLines = 2
+        TextLabel.textAlignment = .center
+        view.addSubview(TextLabel)
+
+        NSLayoutConstraint.activate([
+            TextLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            TextLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            TextLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
     }
-
-
 }
 
+#if DEBUG
+
+import SwiftUI
+
+struct ViewController_Preview: PreviewProvider {
+    static var previews: some View = Preview(
+        for: ViewController()
+    )
+}
+
+#endif
